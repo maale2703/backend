ae10acfd3bec211a1917750af7f63112930fba4d
maryana vitoria <160244232+maale2703@users.noreply.github.com>
2026-04-28 11:54:46 +0000

    versao1

---
+++ objeto.js
@@ -0,0 +1,16 @@
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false
+}
+
+console.log("Material escolhido:", bancoAutomotivo.material);
+
+bancoAutomotivo.concluido = true;
+console.log(bancoAutomotivo);
+
+const lojaCosmético = {
+    material: "Cosméticos"
+   
+}
