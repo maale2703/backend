d20184b554eb8c86fadde5e4e7cdeca7f71f9b96
maryana vitoria <160244232+maale2703@users.noreply.github.com>
2026-04-29 12:23:25 +0000

    versao1

---
+++ pontuacao.js
@@ -0,0 +1,19 @@
+const nomeJogador = "alice"
+let faseAtual = 3;
+let vidasRestantes = 2;
+
+let pontosPorMissao = [85, 90, 60, 100];
+
+//pontosPorMissao.length
+let pontuacaoTotoal = 0;
+for (let i = 0; i < pontuacoes.length; i++) {
+    pontuacaoTotal = pontuacaoTotal + pontuacoes [i]
+}
+
+//Calculando a media  
+let media = pontuacaoTotal / pontuacoes.length;
+
+let passouDeFase = (media >= 75 && vida > 0) || (fase === 1);
+
+console.log(`Seja bem vinda: ${nomeJogador}!`);
+console.log(pontosPorMissao);
